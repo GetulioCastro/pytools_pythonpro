ee1675ce3a4040725b5d17db131c7be7481fc902
Getúlio <augustoge71@gmail.com>
2020-07-01 11:16:58 -0300

    Corrigido o BUG na publicação do PyPi.
    Release do projeto.

--- setup.py
+++ setup.py
@@ -102,7 +102,7 @@ def find_package_data(
 
 
 PACKAGE = "libpythonpro"
-NAME = "libpythonpro"
+NAME = "libpythonpro-gc"
 DESCRIPTION = "Exercício de aprendizagem referente criação de projetos na linguagem Python, Curso Python Pro."
 AUTHOR = "Getulio Castro"
 AUTHOR_EMAIL = "augustoge71@gmail.com"
@@ -117,7 +117,7 @@ setup(
     long_description_content_type='text/markdown',
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
-    license=read('LICENSE'),
+    license='GNU AFFERO GENERAL PUBLIC LICENSE',
     url=URL,
     packages=find_packages(exclude=["tests.*", "tests"]),
     package_data=find_package_data(PACKAGE, only_in_packages=False),
